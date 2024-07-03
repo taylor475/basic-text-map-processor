a786b206a34f5f1efafd67d513bc47b24be04fe2
Taylor Simpson <tss72@byu.edu>
2024-07-03 17:56:54 -0600

    feat: add line processing function

--- index.ts
+++ index.ts
@@ -1,1 +1,22 @@
-const fs = require('node:fs')
+const events = require('events')
+const fs = require('fs')
+const readline = require('readline')
+
+async function processLineByLine() {
+    try {
+        const rl = readline.createInterface({
+            input: fs.createReadStream('./index.ts'),
+            crlfDelay: Infinity
+        })
+
+    rl.on('line', (line) => {
+        console.log(`Line from file: ${line}`)
+    })
+
+    await events.once(rl, 'close')
+
+    console.log('Reading file line by line with readline done.')
+    } catch (err) {
+        console.error(err)
+    }
+}
