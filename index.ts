f3e1234cf7796a83bcde8f1344533afe8c641e71
Taylor Simpson <tss72@byu.edu>
2024-07-03 18:03:36 -0600

    feat: modify line reading function

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ const events = require('events')
 const fs = require('fs')
 const readline = require('readline')
 
+// Function for reading each line of a file into an array
 async function processLineByLine() {
     try {
         const rl = readline.createInterface({
@@ -9,13 +10,16 @@ async function processLineByLine() {
             crlfDelay: Infinity
         })
 
-    rl.on('line', (line) => {
-        console.log(`Line from file: ${line}`)
-    })
+        const lines: String[] = []
+        rl.on('line', (line) => {
+            console.log(`Line from file: ${line}`)
+            lines.push(line)
+        })
 
-    await events.once(rl, 'close')
+        await events.once(rl, 'close')
 
-    console.log('Reading file line by line with readline done.')
+        console.log('Reading file line by line with readline done.')
+        return lines
     } catch (err) {
         console.error(err)
     }
